6f8f6039be09f6ba84c468ffc6a1e7f6965f4465
kentokanazawa@yahoo.com <kentokanazawa@yahoo.com>
2020-01-13 14:09:53 -0500

    edited Image component to take any filename inside images folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,23 +8,34 @@ import SEO from "../components/seo"
 const IndexPage = ({ data }) => {
   useStaticQuery(
     graphql`
-      query SiteMeta {
+      query AuthorQuery {
         site {
           siteMetadata {
             title
           }
         }
+        allAuthorJson {
+          edges {
+            node {
+              firstName
+              bannerImage
+            }
+          }
+        }
       }
     `
   )
+  console.log(data.allAuthorJson.edges[0].node.bannerImage)
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Welcome To A Page For {data.site.siteMetadata.title}</h1>
-      <p>Welcome to your new Gatsby site.</p>
+      <h1>
+        Welcome To A Page For {data.allAuthorJson.edges[0].node.firstName}
+      </h1>
+      <p> {data.site.siteMetadata.title}</p>
       <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
+        <Image alt={"hello"} filename={data.allAuthorJson.edges[0].node.bannerImage}/>
       </div>
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
